930711f3880e603f1dcb503e2b1852386424c9b9
kulka <kulkarni.rohitva@gmail.com>
2023-06-06 20:33:21 -0400

    updated for 2023-2024

--- src/components/constants/index.js
+++ src/components/constants/index.js
@@ -1,15 +1,9 @@
 export const team_home = [
     {
-        id: "leader-2",
-        img:"https://drive.google.com/uc?export=view&id=1bahGmiG9TJrQvqpCT9fXVY1DxORAKtY8",
-        title: "William Tuck, Co-president Cyber Club",
-        content: "Will has been involved in cybersecurity efforts at Freedom for four years. He loves to code and compete in cybersecurity competitions. He plans to study computer science in college but is also considering cybersecurity.",
-    },
-    {
         id: "leader-1",
-        img:"https://www.wikihow.com/images/6/61/Draw-a-Cartoon-Man-Step-15.jpg",
-        title: "Arjun Rao, Co-president Cyber Club",
-        content: "Hi I am Arjun",
+        img:"https://drive.google.com/uc?export=view&id=19wfPpRgN5Vocm7iRKDqemhbZ20YhBY_g",
+        title: "Rohit Kulkarni, president Cyber Club",
+        content: "Rohit is a high school student at Freedom High School with an aptitude for STEM. As an aspiring computer scientist interested in the intersection of machine learning and biology, he is always ready to learn and challenge himself in a growingly technology-dependent world. Along with school, he likes educating himself about the newest advancements in technology, playing tennis, and riffing on the guitar.",
     },
 ]
 
@@ -38,25 +32,37 @@ export const team_ml = [
 export const team_aws = [
     {
         id:"leader-1",
-        img:"https://drive.google.com/uc?export=view&id=1Lw6PQQ2lnzG-Hs-NNgioY6x7jDdoYPvo",
-        title: "Sidd Ravi, Co-president of AWS club",
-        content: "Sidd is a senior in high school who has committed to Virginia Tech where he will be pursuing a Bachelor’s in Computer Science. His interests include programming and cloud technologies. He is also enjoys participating in hackathon/cybersecurity competitions.",
+        img:"",
+        title: "Saamarth Attray, President of AWS club",
+        content: "",
     },
     {
         id:"leader-2",
-        img:"https://www.wikihow.com/images/6/61/Draw-a-Cartoon-Man-Step-15.jpg",
-        title: "Afdhal Nizamudheen, AWS Officer",
-        content: "Hi",
+        img:"",
+        title: "Suhas Adavelly, Secretary of AWS Club",
+        content: "",
     },
 ]
 
 export const team_cybersec = [
     {
         id:"leader-1",
-        img:"https://www.wikihow.com/images/6/61/Draw-a-Cartoon-Man-Step-15.jpg",
-        title: "Nikhil Boyalla, Cybersecurity Officer",
-        content: "Hi",
+        img:"",
+        title: "Anjana Kumar, Cybersecurity President",
+        content: "",
     },
+    {
+        id:"leader-2",
+        img:"",
+        title: "Shehab Salem, Cybersecurity Vice President",
+        content: "",
+    },
+    {
+        id:"leader-3",
+        img:"",
+        title: "Riya Pasupulati, Cybersecurity Secretary",
+        content: "",
+    }
 ]
 
 
@@ -87,7 +93,7 @@ export const projects_cybersec = [
         id: "project-1",
         img: "https://img.freepik.com/premium-vector/operation-project-icon-comic-style-gear-process-vector-cartoon-illustration-white-isolated-background-technology-produce-business-concept-splash-effect_157943-5822.jpg?w=2000",
         title: "Mini-Capture the Flag (CTF)",
-        content: "about the CTF",
+        content: "Capture the flag event designed for all students.",
     }
 ]
 
@@ -138,7 +144,7 @@ export const contact_home = [
     id: "contact-1",
     img:"https://www.pngkey.com/png/full/9-96916_download-email-icon-transparent-background.png",
     title: "President Officer Emails",
-    content: "William Tuck: 832631@lcps.org",
+    content: "Rohit Kulkarni: 922504@lcps.org",
     }
 ]
 
@@ -169,7 +175,7 @@ export const contact_cybersec = [
         id: "contact-1",
         img: "https://www.pngkey.com/png/full/9-96916_download-email-icon-transparent-background.png",
         title: "Cybersecurity Officer Emails",
-        content: "No contact yet",
+        content: "Anjana Kumar: 1035151@lcps.org, Shehab Salem: 1044596@lcps.org, Riya Pasupulati: 854076@lcps.org",
     }
 ]
 
@@ -178,6 +184,6 @@ export const contact_aws = [
         id: "contact-aws",
         img:"https://www.pngkey.com/png/full/9-96916_download-email-icon-transparent-background.png",
         title: "AWS Officer Emails",
-        content: "Sidd Ravi: 840953@lcps.org",
+        content: "Saamarth Attray: 901395@lcps.org, Suhas Adavelly: 901997@lcps.org",
     }
 ]
